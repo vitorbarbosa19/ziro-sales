e12e0ea1b1c467c63c2a281ff559d94620d73962
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-01-30 20:34:21 -0800

    trying one more time to config webpack for netlify

--- src/components/RegisterNewSale/utils/fetchInitialData.js
+++ src/components/RegisterNewSale/utils/fetchInitialData.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const fetchInitialData = async () => {
-	const supplierSheet = await axios.get(process.env.SUPPLIERS_SHEET_URL)
-	const resellerSheet = await axios.get(process.env.RESELLERS_SHEET_URL)
+	console.log(process.env.SUPPLIERS_SHEET_URL)
+	const supplierSheet = await axios.get(`${process.env.SUPPLIERS_SHEET_URL}`)
+	const resellerSheet = await axios.get(`${process.env.RESELLERS_SHEET_URL}`)
 	const suppliers = supplierSheet.data.values.map( (supplierInfo) => supplierInfo[0] ).splice(1).sort()
 	const resellers = resellerSheet.data.values.map( (resellerInfo) => resellerInfo[0] ).splice(1).sort()
 	const status = 'FETCH_OK'
