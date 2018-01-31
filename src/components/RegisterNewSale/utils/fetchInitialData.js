bdcc5f9ccc833e9362cca64da9244ce90c8e0053
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-01-31 12:16:22 -0800

    netlify's automatic deploys are now working properly

--- src/components/RegisterNewSale/utils/fetchInitialData.js
+++ src/components/RegisterNewSale/utils/fetchInitialData.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const fetchInitialData = async () => {
-	console.log(process.env.SUPPLIERS_SHEET_URL)
 	const supplierSheet = await axios.get(`${process.env.SUPPLIERS_SHEET_URL}`)
 	const resellerSheet = await axios.get(`${process.env.RESELLERS_SHEET_URL}`)
 	const suppliers = supplierSheet.data.values.map( (supplierInfo) => supplierInfo[0] ).splice(1).sort()
