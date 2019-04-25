b10aa84d54c179b6624449c92281f2c24500b1b0
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-04-24 23:09:49 -0700

    minor fix

--- src/components/RegisterNewSale/Currency/utils/formatInput.js
+++ src/components/RegisterNewSale/Currency/utils/formatInput.js
@@ -1,8 +1,6 @@
 const formatInput = (input, symbol) => {
-	if (input === 0)
-		return `${symbol} ${parseFloat(0).toFixed(2)}`
 	if (!parseInt(input))
-		return ''
+		return `${symbol} ${parseFloat(0).toFixed(2)}`
 	return `${symbol} ${parseFloat(input / 100).toFixed(2)}`
 }
 
